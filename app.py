a40c17d5963ca9b9ed63314aced0391d80d3248f
karthick <vikramkarthick2001@gmail.com>
2024-10-29 20:14:41 +0530

    update

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,jsonify,url_for,render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+app=Flask(__name__)
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=np.array(list(data.values())).reshape(1,-1)
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=np.array(data).reshape(1,-1)
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)

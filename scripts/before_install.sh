63a552f93a405e4a8cb5b8f62ec8a58c5385e485
karthick <vikramkarthick2001@gmail.com>
2024-10-30 15:43:32 +0530

    editiedbeforesh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,26 +3,24 @@
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
     echo "Docker not found. Installing Docker..."
-    sudo apt-get update -y
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt-get install -y docker-ce
+    sudo yum update -y
+    sudo yum install -y docker
 
-    # Start Docker
+    # Start and enable Docker
     sudo systemctl start docker
     sudo systemctl enable docker
-    sudo apt-get install awscli -y
 
-    # Add the current user to Docker group
-    sudo usermod -aG docker ubuntu
+    # Add the current user to the Docker group
+    sudo usermod -aG docker ec2-user
     echo "Docker installed successfully."
-fi
+else
     echo "Docker Found"
-
-if ! command -v aws &>/dev/null; then
-    echo "AWS CLI not found. Installing AWS CLI..."
-    sudo apt-get update -y
-    sudo apt-get install awscli -y
 fi
+
+# Check if AWS CLI is installed
+if ! command -v aws &> /dev/null; then
+    echo "AWS CLI not found. Installing AWS CLI..."
+    sudo yum install -y aws-cli
+else
     echo "AWS CLI is already installed."
+fi

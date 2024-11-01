c81198b10ae280092ec4ac674e96ebcc64499e31
karthick <vikramkarthick2001@gmail.com>
2024-11-01 11:01:31 +0530

    stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,23 +2,6 @@
 
 echo "This script stops running Docker containers, removes them, and deletes Docker images."
 
-# Stop running containers if any exist
-if [ "$(sudo docker ps -q)" ]; then
-    sudo docker stop $(sudo docker ps -q)
-else
-    echo "No running containers to stop."
-fi
-
-# Remove stopped containers if any exist
-if [ "$(sudo docker ps -a -q)" ]; then
-    sudo docker rm $(sudo docker ps -a -q)
-else
-    echo "No containers to remove."
-fi
-
-# Remove images if any exist
-if [ "$(sudo docker images -q)" ]; then
-    sudo docker rmi $(sudo docker images -q)
-else
-    echo "No images to remove."
-fi
+sudo docker stop $(sudo docker ps -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi $(sudo docker images -q)
